8fac11549ae67bade9ecd2f89464a1120d42babc
TusharJadhav <tushar.laxman@inteliquity.in>
2023-01-18 18:04:14 +0530

    add CrudAdo project

---
+++ Example/CRUDADO/Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using CRUDADO.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+using Xunit.Sdk;
+
+namespace CRUDADO.Models
+{
+    public class Teacher
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [SkillsValidate(Allowed = new string[] { "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web Forms" }, ErrorMessage = "You skills are invalid")]
+        public string Skills { get; set; }
+        [Range(5, 50)]
+        public int TotalStudents { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public DateTime AddedOn { get; set; }
+    }
+}
